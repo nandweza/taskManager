a6006957d22bb06475d55f2cfc7ecb2157959ec7
nandweza <allannandweza@gmail.com>
2023-06-10 09:56:11 +0300

    organised project structure, added task models and routes

--- api/app.js
+++ api/app.js
@@ -2,12 +2,23 @@ const express = require("express");
 
 const app = express();
 
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const taskRoute = require("./routes/task");
 
 const port = 8000 || process.env.PORT;
+dotenv.config();
 
-app.get("/", (req, res) => {
-    res.send("Hello");
-});
+mongoose.connect("mongodb://localhost:27017/taskManagerDB", {
+    useNewUrlParser: true
+})
+.then(() => console.log("DB Connected Successfully"))
+.catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/task/", taskRoute);
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`);
