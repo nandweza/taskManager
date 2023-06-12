23841df32dafeb70882650aa36c182a0faa5b15a
nandweza <allannandweza@gmail.com>
2023-06-12 08:13:40 +0300

    created task routes

--- api/models/Task.js
+++ api/models/Task.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const taskSchema = new mongoose.Schema(
     {
         title: String,
-        content: String
+        content: Array
     }, 
     { timestamps: true }
 );
--- api/routes/task.js
+++ api/routes/task.js
@@ -1,9 +1,67 @@
 const express = require("express");
 const router = express.Router();
 const Task = require("../models/Task");
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-    res.send("Hello, World!!!!");
+
+//task routes
+router.route("/")
+.post(async (req, res) => {
+    if (req.user) {
+        const newTask = new Task(req.body);
+
+        try {
+            const savedTask = await newTask.save();
+            res.status(201).json(savedTask);
+        } catch(err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed");
+    }
+})
+
+.get(async (req, res) => {
+    if (req.user) {
+        try {
+            const tasks = await Task.find().sort({ createdAt: -1 });
+            res.status(201).json(tasks); 
+        } catch(err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("Not allowed");
+    }
 });
 
+router.route("/:id")
+.put(async (req, res) => {
+    if (req.user) {
+        try {
+            const updatedTask = await Task.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            }, { new: true });
+            res.status(201).json(updatedTask);
+        } catch(err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json(" User not allowed");
+    }
+})
+
+.delete(async (req, res) => {
+    if (req.user) {
+        try {
+            await Task.findByIdAndDelete(req.params.id);
+            res.status(201).json("Task deleted successfully!!!");
+        } catch(err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("User not allowed");
+    }
+})
+
 module.exports = router;
+
